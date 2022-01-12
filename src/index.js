205c1708efaf361867de9fa3d607460d2789c332
Wisarut41372 <mos0880040156@gmail.com>
2022-01-12 19:03:56 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,50 +1,24 @@
-require('dotenv').config({ path:'./config.env'});
-const express  = require('express');
-const bodyParser  = require('body-parser');
-const cors  = require('cors');
-const res = require('express/lib/response');
-const req = require('express/lib/request');
-const port = process.env.PORT || 4002 ;
+require('dotenv').config({ path: './config.env'});
 
-const app =express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const port = process.env.PORT || 4002
+
+const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended : false
+    extended: false
 }));
 
-const productRoute = require("./routes/productRoute");
-const productRoute = require("./routes/employeeRoute");
-app.use("/product",productRoute);
-app.use("/employee",employeeRoute);
+const employeeRoute = require("./routes/employeeRoute");
 
 
-app.listen(port,()=>{
-    console.log(`App is running on port ${port}`);
+app.use("/employee", employeeRoute);
+
+
+app.listen(port, ()=>{
+    console.log(`App is runnung on port ${port}`);
 });
-
-
-//Routing Table
-// app.get("/",(req, res)=>{
-//     res.send("Hello from index");
-// });
-
-// app.get("/login",(req, res)=>{
-//     res.send("Hello from login");
-// });
-
-// app.post("/register",(req, res)=>{
-//     console.log(req.body.name);
-//     console.log(req.body.email);
-//     res.send("Hello from register");
-// });
-
-// app.get("/product/:id",(req,res)=>{
-//     res.send(`Product ID : ${req.params.id}`);
-// });
-
-// app.get("/product/:name",(req,res)=>{
-//     res.send(`Product name : ${req.params.id}`);
-// });
-
 
